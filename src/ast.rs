26fd347aac5434f44252da5609d2df5e65a06a53
Samcolserra <samcolserra@gmail.com>
2023-04-08 02:24:58 +0300

    Added print method for Expression enum

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,23 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum Expr {
     Number(i32),
     Op(Box<Expr>, OpCode, Box<Expr>),
+}
+impl Expr {
+    pub fn pretty_print(&self, num: usize) {
+        print!("{}", "_".repeat(num));
+
+        match self {
+            Expr::Number(number) => println!("{}", number),
+            Expr::Op(lhs, op, rhs) => {
+                println!("{}", op);
+                lhs.pretty_print(num + 1);
+                rhs.pretty_print(num + 1);
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -11,4 +27,15 @@ pub enum OpCode {
     Add,
     Sub,
     Exp,
+}
+impl Display for OpCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OpCode::Mul => write!(f, "*"),
+            OpCode::Div => write!(f, "/"),
+            OpCode::Add => write!(f, "+"),
+            OpCode::Sub => write!(f, "-"),
+            OpCode::Exp => write!(f, "^"),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ lalrpop_mod!(pub example, "/example.rs");
 
 fn main() {
     let data = fs::read_to_string("./src/text_to_parse.txt").unwrap();
-    let result = example::ExprParser::new().parse(&data);
-    println!("parse result: {:?}", result.unwrap());
+    let result = example::ExprParser::new().parse(&data).unwrap();
+    result.pretty_print(0);
 }
