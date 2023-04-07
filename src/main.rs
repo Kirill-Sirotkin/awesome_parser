a4941f29ee0b5db9dd220b9b6ca9c33563e9d9b4
Samcolserra <samcolserra@gmail.com>
2023-04-08 00:43:41 +0300

    AST implementation

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ use std::fs;
 #[macro_use]
 extern crate lalrpop_util;
 
-lalrpop_mod!(pub example);
+lalrpop_mod!(pub example, "/example.rs");
 
 fn main() {
     let data = fs::read_to_string("./src/text_to_parse.txt").unwrap();
-    example::ProgramExpressionSequenceParser::new()
-        .parse(&data)
-        .unwrap();
-    // println!("parse result: {}", result.unwrap());
+    let result = example::ExprParser::new().parse(&data);
+    println!("parse result: {:?}", result.unwrap());
 }
